fe25d2de1ad10c1ea46eb9879faf61d2b41d31f5
KhalyavinSergey <44732519+KhalyavinSergey@users.noreply.github.com>
2018-11-20 19:37:16 +0300

    Add files via upload

---
+++ zadacha1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           double sum = 0;//сумма
+            int n = 1;//количество проходов
+            double temp;//предыдущий результат, для проверки точности
+            
+            do
+            {
+                temp = sum;
+                sum += Math.Pow((-1), n + 1) / Math.Pow(2*n, 3);
+                n++; 
+            }
+            while (Math.Abs (sum-temp) > 0.001);
+            Console.WriteLine("Сумма ряда: " + sum );
+            Console.ReadLine();
+        }
+       
+    }
+}
